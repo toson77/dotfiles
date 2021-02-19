2c186ead94d0a236b974c2c11464f22303508423
Noriyuki <norifxsc@gmail.com>
2021-02-19 21:49:13 +0900

    [fix] install.sh nottest

--- bin/install.sh
+++ bin/install.sh
@@ -3,3 +3,34 @@
 # symlink
 ln -sf ~/init.vim ~/.config/nvim/init.vim
 
+# vimplug install
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#package install
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential
+
+#install n
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install -g n
+sudo n lts
+sudo apt purge nodejs npm
+
+#install python3 pyenv 
+sudo apt update
+sudo apt install git gcc make zlib1g-dev libffi-dev libbz2-dev libssl-dev libreadline-dev libsqlite3-dev python3-tk tk-dev
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+
+#install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install cargo-edit
+cargo install cargo-watch
+cargo install evcxr_repl
+
+#install haskell
+sudo apt-get install haskell-platform
