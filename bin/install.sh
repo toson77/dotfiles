e30eeac116045ca13e3a0360e75aeb0cb2ce7f96
Noriyuki <norifxsc@gmail.com>
2021-02-20 19:45:57 +0900

    [fix] install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -23,7 +23,7 @@ sudo apt purge nodejs npm -y
 
 #install python3 pyenv
 sudo apt update
-sudo apt install git gcc make zlib1g-dev libffi-dev libbz2-dev libssl-dev libreadline-dev libsqlite3-dev python3-tk tk-dev -y
+sudo apt install gcc zlib1g-dev libffi-dev libbz2-dev libssl-dev libreadline-dev libsqlite3-dev python3-tk tk-dev -y
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
 
